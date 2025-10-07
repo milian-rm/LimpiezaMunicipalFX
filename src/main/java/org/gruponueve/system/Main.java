b33f83cadf44cef9ec1af589f5de5dc2d9701ab5
DevJosL <jmoran-2022388@kinal.edu.gt>
2025-10-06 20:17:09 -0600

    Primer commit, archivos iniciales

---
+++ src/main/java/org/gruponueve/system/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.gruponueve.system;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author josel
+ */
+public class Main extends Application{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
