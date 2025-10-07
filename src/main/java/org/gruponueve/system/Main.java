4d01b4c1220a62a9ddc468671c856a7bfd8305c9
DevJosL <jmoran-2022388@kinal.edu.gt>
2025-10-06 22:44:33 -0600

    T06/ Creación del Main inicial

--- src/main/java/org/gruponueve/system/Main.java
+++ src/main/java/org/gruponueve/system/Main.java
@@ -4,7 +4,12 @@
 
 package org.gruponueve.system;
 
+import java.util.logging.Logger;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -12,13 +17,45 @@ import javafx.stage.Stage;
  * @author josel
  */
 public class Main extends Application{
+    private Stage stage;
+    private static int ANCHO = 1500;
+    private static int ALTO = 800;
+    private static String URL = "/view/";
+    private Logger logger;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Prueba funcionamiento");
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.stage = stage;
+        /*FXMLLoader cargador = new FXMLLoader(getClass().getResource(
+                "/view/InicioView.fxml"));
+        Parent raiz = cargador.load();
+        Scene escena = new Scene(raiz);
+        stage.setScene(escena);
+        stage.setMaximized(true);*/
+        stage.show();
+    }
+
+    public FXMLLoader cambioEscena(String fxml){
+        FXMLLoader cargardorFXML = null;
+        try {
+            cargardorFXML = new FXMLLoader(getClass().getResource(URL+fxml));
+            Parent archivoFXML = cargardorFXML.load();
+            Scene escena = new Scene(archivoFXML,ANCHO,ALTO);
+            stage.setScene(escena);
+            //Prueba
+            stage.setMaximized(true);
+            //----
+            stage.setTitle("ME ATRAPASTE ME TUVISTE ENTRE TUS BRAZOS, ME ENSEÑASTE LO "+
+                            "INHUMANO Y LO INFELIZ QUE PUEDO SEEEER");
+        } catch (Exception e) {
+            logger.info("ERROR AL CAMBIAR " + e.getMessage());
+            e.printStackTrace();
+        }
+        return cargardorFXML;
     }
 }
